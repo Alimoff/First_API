f3ad8712e3870f466cc185c7b2ee2de29647958f
Alimoff <sobiriddin@gmail.com>
2022-07-03 10:24:11 +0500

    add:videos model

--- youtube/models.py
+++ youtube/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Videos(models.Model):
+    name = models.CharField("Video", max_length=200)
+    text = models.TextField("Description:")
+
+    # More info
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Video"
+        verbose_name_plural = "More Videos"
